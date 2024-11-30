b5bad4609a0393db5431598a6a5b4ea1a3a6b8b2
Nate River <river177169@proton.me>
2024-12-01 04:36:29 +0530

    added envs

--- src/consts/env.rs
+++ src/consts/env.rs
@@ -10,6 +10,11 @@ pub static CC: LazyLock<String>=LazyLock::new(|| {
   .unwrap_or_else(|_| DEFAULT_CC.to_owned())
 });
 
+pub static DEFAULT_LINKER: &str="ar";
+pub static LINKER: LazyLock<String>=LazyLock::new(|| {
+  env::var("LINKER")
+  .unwrap_or_else(|_| DEFAULT_LINKER.to_owned())
+});
 
 
 
